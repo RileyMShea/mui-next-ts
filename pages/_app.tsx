d0c993d742cd59d5e0375ab3dab202b0a9602460
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2020-06-17 16:00:51 -0600

    refactor: Convert _app to functional component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,30 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
-import App from 'next/app';
-import Head from 'next/head';
-import React from 'react';
-
 import { AuthProvider } from 'hooks/auth-context';
 import { UserProvider } from 'hooks/user-context';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import React, { useEffect } from 'react';
 
-class MyApp extends App {
-  componentDidMount() {
+export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
-  }
+  }, []);
 
-  render() {
-    const { Component, pageProps } = this.props;
-
-    return (
-      <>
-        <Head>
-          <title>MUI + Next.js</title>
-        </Head>
-        <UserProvider initialUser={pageProps.user}>
-          <AuthProvider>
-            <CssBaseline />
-            <Component {...pageProps} />
-          </AuthProvider>
-        </UserProvider>
-      </>
-    );
-  }
+  return (
+    <>
+      <Head>
+        <title>MUI + Next.js</title>
+      </Head>
+      <UserProvider initialUser={pageProps.user}>
+        <AuthProvider>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </AuthProvider>
+      </UserProvider>
+    </>
+  );
 }
-
-export default MyApp;
